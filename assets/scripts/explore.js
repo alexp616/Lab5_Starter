bb5a4fa46241138ccb897af5fea8d3c05162b773
alexp616 <alpan@ucsd.edu>
2025-05-06 20:21:20 -0700

    finished explore webpage

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,57 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+let getText = () => document.getElementById('text-to-speak').value;
+
+let synth;
+let voiceSelect;
+let voices = [];
+function populateVoiceList() {
+  synth = window.speechSynthesis;
+  voiceSelect = document.querySelector("select");
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; ++i) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
 }
+
+function init() {
+  const speakButton = document.getElementsByTagName("button")[0];
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+  
+  speakButton.addEventListener("click", () => {
+    speechSynthesis.cancel();
+    let utterance = new SpeechSynthesisUtterance(getText());
+
+    let selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterance.voice = voices[i];
+      }
+    }
+
+    let img = document.querySelector('img');
+    utterance.onstart = () => {
+      img.src = "assets/images/smiling-open.png";
+    }
+
+    utterance.onend = () => {
+      img.src = "assets/images/smiling.png";
+    }
+
+    speechSynthesis.speak(utterance);
+  });
+
+  return;
+}
+
